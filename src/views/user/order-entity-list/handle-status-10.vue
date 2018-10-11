1f717a50cfeab865f387053fe25d94b2095b7abe
TheInlaidHarp <theinlaidharp@gmail.com>
2018-10-11 09:12:58 +0800

    手机钱包app

---
+++ src/views/user/order-entity-list/handle-status-10.vue
@@ -0,0 +1,9 @@
+<template>
+	<div slot="footer" class="order_list--footer_btn">
+		<van-button size="small" @click="$emit('handle', 'cancelOrder')">取消</van-button>
+		<van-button size="small" type="danger" @click="$emit('handle', 'toPay')">1</van-button>
+	</div>	
+</template>
+
+<script>
+</script>
